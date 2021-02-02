d7b75385cd40ed4432da1639f54c5ea5a3ac22c0
ProneetSeth <proneetofficial111@gmail.com>
2021-02-02 23:35:42 +0530

    memoized code was missing...so added

---
+++ Climb_Stair_Tabulated/ClimbStairMemoized.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class ClimbStairMemoized {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        System.out.println(climbStairsDP(n));
+
+    }
+    //Tabulated
+    public static int climbStairsDP(int n) {
+        int qb[] = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+
+            if (i == 0) {
+                //dest
+                qb[0] = 1;
+                continue;
+            }
+
+            int tways = 0;
+
+            if (i >= 1) {
+                //length = 1
+                tways += qb[i - 1];
+            }
+
+            if (i >= 2) {
+                //length = 2
+                tways += qb[i - 2];
+            }
+            if (i >= 3) {
+                //length = 3
+                tways += qb[i - 3];
+            }
+
+
+            qb[i] = tways;
+            continue;
+        }
+        return qb[n];
+
+    }
+}
